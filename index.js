c2020c58594d5fabcbebe587f6bc6f53b7e0e5fe
bamdart <bamdart@gmail.com>
2020-08-29 19:42:36 +0800

    udpate snow animation, fix web width for ipad

--- index.js
+++ index.js
@@ -1,31 +1,41 @@
-const limitTime = 20 * 1000;
-const stepSnowNumber = Math.min(3 + Math.floor(screen.width / 150), 15);
+const limitTime = 30 * 1000;
+const stepSnowNumber = Math.min(3 + Math.floor(screen.width / 150), 10);
 var totalTime = 0;
 
 document.body.onload = snowing;
 
+function random(min = 0, max, float = true){
+    if(float){
+        return min + Math.random() * (max - min);
+    } else{
+        return min + Math.floor(Math.random() * (max - min + 1) - 0.0001)
+    }
+}
+
 function snowing(){
-    const snowNumber = Math.floor(Math.random() * stepSnowNumber);
+    const snowNumber = random(1, stepSnowNumber, float = false);
     for(let i = 0; i < snowNumber;i++){
         const widthRatio = window.devicePixelRatio || 1;
-        const left = Math.floor(Math.random() * screen.width * widthRatio);
-        const heightShift = Math.floor(Math.random() * 50)
-        const size = 2 + Math.floor(Math.random() * 5);
-        const floatAnimation = 1 + Math.floor(Math.random() * 12 - 0.0001);
-        const floatAnimationTime = 10 + Math.floor(Math.random() * 10 - 0.0001);
-        const snowTransparent = 0.3 + Math.random() * 0.5;
+        const left = random(max = screen.width * widthRatio, float = false);
+        const heightShift = random(-25, 25, float = false);
+        const fallAnimationTime = random(20, 30);
         
         let box = document.createElement("div"); 
         document.body.appendChild(box);
         box.className = "snow-box";
-        box.style = "left: " + left + "px; margin-top: " + heightShift + "px;";
+        box.style = "left: " + left + "px; margin-top: " + heightShift + "px; animation: fall " + fallAnimationTime + "s linear infinite;";
+
+        const size = random(2, 7, float = false);
+        const floatAnimation = random(1, 12, float = false);
+        const floatAnimationTime = random(10, 20);
+        const snowTransparent = random(0.3, 0.8);
 
         let snow = document.createElement("div");
         box.appendChild(snow);
         snow.className = "snow";
         snow.style = "height: " + size + "px; width: " + size + "px; background-color: rgba(255, 255, 255, " + snowTransparent + "); animation: float-" + floatAnimation + " " + floatAnimationTime + "s ease-in-out infinite alternate-reverse;";
     }
-    const delay = Math.random() * 1000;
+    const delay = random(max = 1000, float = false)
     totalTime += delay;
     if(totalTime < limitTime){
         setTimeout(() => snowing(), delay);
